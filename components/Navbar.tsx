0d9a3520ffac2785c752d76f3ae0f3442e59ffaf
Unnat <unnatshrestha123@gmail.com>
2025-04-09 17:43:16 +0545

    Initial commit for new repository

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/logotat.png" alt="logo" width={200} height={50} />
+        <Image src="/logotat.jpg" alt="logo" width={200} height={50} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
